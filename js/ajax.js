cb152dc71c9e547549d315024c54a0cbba946e0d
CristianTafur249 <cristian2191970@correo.uis.edu.co>
2021-09-19 11:53:04 -0500

    Update ajax.js

--- js/ajax.js
+++ js/ajax.js
@@ -1,1 +1,15 @@
-console.log('correcto')
+const formulario = document.getElementById('formulario1')
+const inputs = document.querySelectorAll('#formulario1 input')
+
+const validarF = (e) => {
+    console.log(e.target.name)
+}
+
+inputs.forEach((input)=>{
+    input.addEventListener('keyup', validarF)
+    input.addEventListener('blur', validarF)
+})
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+})
