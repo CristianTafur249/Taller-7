7a5826cba4e0acd22ca892c8db44cd8db373a567
CristianTafur249 <cristian2191970@correo.uis.edu.co>
2021-09-19 22:52:08 -0500

    Update ajax.js

--- js/ajax.js
+++ js/ajax.js
@@ -1,9 +1,38 @@
 const formulario = document.getElementById('formulario1')
 const inputs = document.querySelectorAll('#formulario1 input')
 
-const validarF = (e) => {
-    console.log(e.target.name)
+const expre = {
+    usuario: /^[a-zA-Z0-9\_\-]{4,16}$/,
+    nombre: /^[a-zA-ZÀ-ÿ\s]{4,40}$/
 }
+
+const validarF = (e) => {
+    switch (e.target.name){
+        case "usuario":
+        if(expre.usuario.test(e.target.value)){
+
+        } else{
+            document.getElementById('grupo__usuario').classList.remove('formulario__grupo-incorrecto')
+        }
+        break
+        case "nombres":
+        break
+        case "apellido":
+        break
+        case "Fecha":
+        break
+        case "email":
+        break
+        case "telefono":
+        break
+        case "contraseña":
+        break
+        case "Confirmar":
+        break
+    }
+}
+
+
 
 inputs.forEach((input)=>{
     input.addEventListener('keyup', validarF)
