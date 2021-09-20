c092bcf83037f6420701fc4fa0d9d3ca51caf740
CristianTafur249 <cristian2191970@correo.uis.edu.co>
2021-09-20 10:13:24 -0500

    Update ajax.js

--- js/ajax.js
+++ js/ajax.js
@@ -1,18 +1,18 @@
 const formulario = document.getElementById('formulario1')
-const inputs = document.querySelectorAll('#formulario1 input')
+const usuario = document.getElementById('usuario')
+const email = document.getElementById('email')
+const password = document.getElementById('contraseña')
+const password2 = document.getElementById('Confirmar')
+const names = document.getElementById('nombre')
+const lastName = document.getElementById('apellido')
+const birth = document.getElementById('nacimiento')
+const  tell = document.getElementById('telefono')
 
-const expre = {
-    usuario: /^[a-zA-Z0-9\_\-]{4,16}$/,
-    nombre: /^[a-zA-ZÀ-ÿ\s]{4,40}$/
-}
-
-const validarF = (e) => {
+/* const validarF = (e) => {
     switch (e.target.name){
         case "usuario":
-        if(expre.usuario.test(e.target.value)){
-
-        } else{
-            document.getElementById('grupo__usuario').classList.remove('formulario__grupo-incorrecto')
+        if(){
+            setErrorPor(e.target.name, 'No puede dejar el espacio em blanco')
         }
         break
         case "nombres":
@@ -37,8 +37,61 @@ const validarF = (e) => {
 inputs.forEach((input)=>{
     input.addEventListener('keyup', validarF)
     input.addEventListener('blur', validarF)
-})
+}) */
 formulario.addEventListener('submit', (e) => {
     e.preventDefault()
-
+    checkInputs();
 })
+function checkInputs(){
+    const userVal = usuario.value.trim()
+    const emailVal = email.value.trim()
+    const passVal = password.value.trim()
+    const passVal2 = password2.value.trim()
+    const nameVal = names.value.trim()
+    const lastNameVal = lastName.value.trim()
+    const birthVal = birth.value.trim()
+    const tellVal = tell.value.trim()
+
+    if(userVal == ''){
+        setErrorPor(usuario, 'No puede dejar el usuario en blanco')
+    } else if(!isuser(userVal)) {
+        setErrorPor(usuario, 'El usuario solo puede incluir letras y/o nuemeros, entre 4 y 16 caracteres')
+    }else{
+        setSuccesspor(usuario)
+    }
+    if(emailVal == ''){
+        setErrorPor(email, 'No puede dejar este expacio en blanco')
+    }else if(isEmail(emailVal)){
+        setErrorPor(email, 'No es un email valido')
+    }else{
+        setSuccesspor(email)
+    }
+    if(passVal == ''){
+        setErrorPor(password, 'No se puede dejar este espacio en blanco')
+    }else {
+        setSuccesspor(password)
+    }
+    if(passVal2 == ''){
+        setErrorPor(password2, 'No se puede dejar este espacio en blanco')
+    }else if(passVal2 != passVal){
+        setErrorPor(password2, 'Las contraseñas no coinciden')
+    }else{
+        setSuccesspor(password2)
+    }
+}
+function setSuccesspor(input) {
+    const formControl = input.parentElement
+    formControl.className = 'form-control success'
+}
+function setErrorPor(input,mensage){
+    const formControl = input.parentElement
+    const small = formControl.querySelector('small')
+    formControl.className = 'form-control error'
+    small.innerText = mensage
+}
+function isuser(user) {
+    return /^[a-zA-Z0-9\_\-]{4,16}$/.test(user)
+}
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
